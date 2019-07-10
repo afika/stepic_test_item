f79da416e44d3a55188310445c4925c1bc53fe9d
afika <nastya.tk6990@gmail.com>
2019-07-10 20:33:13 +0700

    change function name

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_button(browser):
+def test_test_browser_language_basket_btn(browser):
     browser.get(link)
     #time.sleep(30)
     try:
