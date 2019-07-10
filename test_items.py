0ed8a6288282d1c83f43178653b8de1db8cd66e9
afika <afika@users.noreply.github.com>
2019-07-10 21:53:30 +0700

    update function name

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_test_browser_language_basket_btn(browser):
+def test_browser_language_basket_btn(browser):
     browser.get(link)
     #time.sleep(30)
     button = browser.find_element_by_css_selector("[class='btn btn-lg btn-primary btn-add-to-basket']")
